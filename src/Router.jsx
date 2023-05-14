2c1574907a06aa8947a7844a2b9030f7aeee4f66
borimong <kimhyunsu11@naver.com>
2023-05-14 21:56:06 +0900

    init:라우터 설정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PageLayout from './components/common/PageLayout';
+
 const Router = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <PageLayout>
+        <Routes>
+          {/*main 페이지 = 최근 청원 페이지여요*/}
+          <Route path="/" element={<div>main(pages 폴더에 페이지 만들어서 import)</div>} />
+          <Route path="/petitionDetail">
+            {/*청원 상세 페이지인데, 각각의 상세 페이지를 구별해주는 것을
+            petitionId라고 정의했어요!
+            아마 서버쌤들께서 배열로 최근 청원 목록 넘겨주실 건데
+            배열 인덱스로 navigate 해주시면 될 것 같습니다!*/}
+            <Route
+              path="/petitionDetail/:petitionId"
+              element={<div>petitionDetail(pages 폴더에 페이지 만들어서 import)</div>}
+            />
+          </Route>
+          <Route path="/myPetition" element={<div>my(pages 폴더에 페이지 만들어서 import)</div>} />
+          <Route
+            path="/createPetition"
+            element={<div>create(pages 폴더에 페이지 만들어서 import)</div>}
+          />
+        </Routes>
+      </PageLayout>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
--- src/components/common/PageLayout.jsx
+++ src/components/common/PageLayout.jsx
@@ -1,5 +1,22 @@
-const PageLayout = () => {
-  return <div></div>;
+import PropTypes from 'prop-types';
+
+const PageLayout = props => {
+  const { children } = props;
+  return (
+    <div>
+      {/*헤더자리*/}
+      {children}
+      {/*푸터자리*/}
+    </div>
+  );
+};
+/*
+이거 propTypes 명시 안 해주니까 에러 나더라구요..
+그래서 prop-types 패키지
+install 하고 타입 명시해 주었습니다~
+*/
+PageLayout.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default PageLayout;
