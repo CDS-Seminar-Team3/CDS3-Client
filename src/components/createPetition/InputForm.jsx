b17cd670719ed778214596df0337163e3cee56a2
borimong <kimhyunsu11@naver.com>
2023-05-16 10:11:16 +0900

    feat: InputForm 컴포넌트 html

--- src/components/createPetition/InputForm.jsx
+++ src/components/createPetition/InputForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { styled } from 'styled-components';
+import theme from '../../styles/theme';
 
 const InputForm = () => {
   return (
-    <div>
+    <St.InputFormWrapper>
       <div>
-        카테고리
+        <h3>카테고리</h3>
         <select>
           <option>카테고리를 선택해주세요.</option>
           <option>편성</option>
@@ -20,21 +22,75 @@ const InputForm = () => {
         </select>
       </div>
       <div>
-        제목 <input />
+        <h3>제목</h3>
+        <input />
       </div>
       <div>
-        내용 <input />
+        <h3>내용</h3>
+        <input className="contentInput" />
       </div>
       <div>
-        링크
-        <div>
+        <h3>링크</h3>
+        <div className="buttonWrapper">
           <input />
           <button>링크 추가</button>
         </div>
       </div>
-      <div>표시된 항목은 필수 입력사항입니다.</div>
-    </div>
+      <div className="InfoRequiredFields">표시된 항목은 필수 입력사항입니다.</div>
+    </St.InputFormWrapper>
   );
 };
 
 export default InputForm;
+
+const St = {
+  InputFormWrapper: styled.div`
+    margin: 0 2rem;
+
+    & > div {
+      display: flex;
+      justify-content: space-between;
+
+      margin-bottom: 2rem;
+
+      & > h3 {
+        ${theme.fonts.body1_bold}
+      }
+
+      & > select {
+        width: 26.8rem;
+      }
+
+      & > .contentInput {
+        height: 9.6rem;
+      }
+
+      & > input {
+        width: 27.1rem;
+        height: 4rem;
+      }
+
+      & > .buttonWrapper {
+        display: flex;
+        flex-wrap: wrap;
+
+        width: 27.1rem;
+
+        & > input {
+          width: 27.1rem;
+          height: 4rem;
+          margin-bottom: 0.8rem;
+        }
+
+        & > button {
+          width: 27.1rem;
+          height: 4rem;
+        }
+      }
+    }
+
+    & > .InfoRequiredFields {
+        padding-top: 0.8rem;
+      }
+  `,
+};
