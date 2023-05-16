495cb0d80d0c8c503a040a2af8ffc4c7d1eec74b
jimyu <aliyah_7@naver.com>
2023-05-17 00:35:28 +0900

    feature: 헤더에 들어갈 요소들 생성

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { styled } from 'styled-components';
+import theme from '../../styles/theme';
+import { ReactComponent as IcMore } from '../../assets/icons/IcMore.svg';
+import { ReactComponent as IcMenu } from '../../assets/icons/IcMenu.svg';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <St.HeaderWrapper>
+      <St.TitleMenu>
+        <img>
+          <IcMenu />
+        </img>
+        <span>KBS 시청자 센터</span>
+      </St.TitleMenu>
+      <St.PageMenu>
+        <span>청원하기</span>
+        <img>
+          <IcMore />
+        </img>
+      </St.PageMenu>
+    </St.HeaderWrapper>
+  );
 };
 
 export default Header;
+
+export const St = {
+  HeaderWrapper: styled.header`
+    height: 4.2rem;
+    width: 37.5rem;
+
+    background-color: ${({ theme }) => theme.colors.white};
+  `,
+  TitleMenu: styled.div`
+    display: flex;
+    align-items: center;
+
+    & > img {
+      width: 3.2rem;
+      height: 3.2rem;
+    }
+    & > span {
+      font-size: 1.6rem;
+      font-weight: 700;
+    }
+  `,
+  PageMenu: styled.div`
+    display: flex;
+    align-items: center;
+
+    & > span {
+      font-size: 1.6rem;
+      font-weight: 700;
+    }
+
+    & > img {
+      width: 3.2rem;
+      height: 3.2rem;
+    }
+  `,
+};
