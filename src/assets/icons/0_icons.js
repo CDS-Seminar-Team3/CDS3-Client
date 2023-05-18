08c0261401a6d06517cd2dfc3c7efbe1ab75f8fa
jimyu <aliyah_7@naver.com>
2023-05-18 09:45:59 +0900

    style: 아이콘 추가

--- src/assets/icons/0_icons.js
+++ src/assets/icons/0_icons.js
@@ -1,4 +1,9 @@
 import { ReactComponent as IcRedDot } from '../icons/IcRedDot.svg';
 import { ReactComponent as IcLinkAdd } from '../icons/IcLinkAdd.svg';
+import { ReactComponent as IcMenu } from '../icons/IcMenu.svg';
+import { ReactComponent as IcShare } from '../icons/IcShare.svg';
+import { ReactComponent as IcProgress } from '../icons/IcProgress.svg';
+import { ReactComponent as IcProgress2 } from '../icons/IcProgress2.svg';
+import { ReactComponent as IcProgress3 } from '../icons/IcProgress3.svg';
 
-export { IcRedDot, IcLinkAdd };
+export { IcRedDot, IcLinkAdd, IcMenu, IcShare, IcProgress, IcProgress2, IcProgress3 };
