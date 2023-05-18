608fbef6202264d7e93257a5972b44954f8ea77d
jimyu <aliyah_7@naver.com>
2023-05-18 22:45:11 +0900

    code review: 아이콘 import 방식 수정

--- src/assets/icons/0_icons.js
+++ src/assets/icons/0_icons.js
@@ -1,9 +1,7 @@
-import { ReactComponent as IcRedDot } from '../icons/IcRedDot.svg';
-import { ReactComponent as IcLinkAdd } from '../icons/IcLinkAdd.svg';
-import { ReactComponent as IcMenu } from '../icons/IcMenu.svg';
-import { ReactComponent as IcShare } from '../icons/IcShare.svg';
-import { ReactComponent as IcProgress } from '../icons/IcProgress.svg';
-import { ReactComponent as IcProgress2 } from '../icons/IcProgress2.svg';
-import { ReactComponent as IcProgress3 } from '../icons/IcProgress3.svg';
-
-export { IcRedDot, IcLinkAdd, IcMenu, IcShare, IcProgress, IcProgress2, IcProgress3 };
+export { ReactComponent as IcRedDot } from '../icons/IcRedDot.svg';
+export { ReactComponent as IcLinkAdd } from '../icons/IcLinkAdd.svg';
+export { ReactComponent as IcMenu } from '../icons/IcMenu.svg';
+export { ReactComponent as IcShare } from '../icons/IcShare.svg';
+export { ReactComponent as IcProgress } from '../icons/IcProgress.svg';
+export { ReactComponent as IcProgress2 } from '../icons/IcProgress2.svg';
+export { ReactComponent as IcProgress3 } from '../icons/IcProgress3.svg';
