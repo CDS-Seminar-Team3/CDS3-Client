d7bc48007c3966588b968075dda01747269480ac
ljh0608 <m98m@catholic.ac.kr>
2023-05-25 04:11:25 +0900

    chore: 테스트코드 삭제

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import theme from '../styles/theme';
 import PetitionList from '../components/common/PetitionList';
 import PageName from '../components/common/PageName';
-import { DATA } from '../constants/data';
 import PetitionHeader from '../components/currentPetition/PetitionHeader';
 import { currentPetitionSelector } from '../recoils/selector';
 
@@ -11,7 +10,7 @@ import { useEffect } from 'react';
 
 const Main = () => {
   const data = useRecoilValue(currentPetitionSelector);
-  // console.log('메인데이터확인' + JSON.stringify(data, 2, null));
+
   useEffect(() => {}, [data]);
 
   return (
@@ -20,7 +19,7 @@ const Main = () => {
         <PageName>이슈 청원</PageName>
         <PetitionHeader></PetitionHeader>
         <PageName button={<St.MyPetitionBtn>나의 청원</St.MyPetitionBtn>}>최근 청원</PageName>
-        <PetitionList data={data}></PetitionList>
+        <PetitionList></PetitionList>
         {/* 페이지네이션을 추가해주세요 */}
       </St.MainWrapper>
     </>
--- src/recoils/selector.js
+++ src/recoils/selector.js
@@ -1,27 +1,10 @@
-import { selectorFamily, selector } from 'recoil';
+import { selector } from 'recoil';
 import useGetCurrentPetition from '../hooks/useGetCurrentPetition';
 
 export const currentPetitionSelector = selector({
   key: 'currentPetitionSelector',
   get: () => {
     const currentPetition = useGetCurrentPetition();
-    // console.log(currentPetition);
     return currentPetition;
   },
-});
-
-export const searchCurrentPetitionSelector = selectorFamily({
-  key: 'searchCurrentPetitionSelector',
-  get:
-    param =>
-    ({ get }) => {
-      const searchPetition = get(currentPetitionSelector);
-      // console.log(searchPetition);
-
-      if (param) {
-        return searchPetition.filter(item => item.title.includes(param));
-      } else {
-        return searchPetition;
-      }
-    },
 });
