d23a0524e9ace653a8b4c05f5aa7a3c06cce511a
borimong <kimhyunsu11@naver.com>
2023-05-25 13:09:56 +0900

    feat: post 후 나의 청원 페이지로 라우팅 및 등록 조건 충족시에만 post 하게 하기

--- src/components/createPetition/PetitionButtons.jsx
+++ src/components/createPetition/PetitionButtons.jsx
@@ -11,14 +11,16 @@ import { styled } from 'styled-components';
 import theme from '../../styles/theme';
 import PropTypes from 'prop-types';
 import usePostPetition from '../../hooks/usePostPetition';
+import { useNavigate } from 'react-router-dom';
 
-const PetitionButtons = ({data}) => {
+const PetitionButtons = ({ data }) => {
   const [isRegister, setIsRegister] = useRecoilState(isRegisterState);
   const category = useRecoilValue(categoryState);
   const title = useRecoilValue(titleState);
   const content = useRecoilValue(contentState);
   const checkCaution = useRecoilValue(checkCautionState);
-  const { isLoading, isError, postPetition} = usePostPetition();
+  const { isLoading, isError, postPetition } = usePostPetition();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (category !== '카테고리를 선택해주세요.' && title !== '' && content !== '' && checkCaution) {
@@ -29,13 +31,18 @@ const PetitionButtons = ({data}) => {
   }, [category, title, content, checkCaution]);
 
   const onClickRegister = () => {
-    postPetition(data);
-  }
+    if (isRegister) {
+      postPetition(data);
+      navigate('/myPetition');
+    }
+  };
 
   return (
     <St.PetitionButtonsWrapper isRegister={isRegister}>
       <button className="cancelButton">취소</button>
-      <button className="registerButton" onClick={onClickRegister}>등록</button>
+      <button className="registerButton" onClick={onClickRegister}>
+        등록
+      </button>
     </St.PetitionButtonsWrapper>
   );
 };
